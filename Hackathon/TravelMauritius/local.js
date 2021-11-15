29fdfe637f315c4521cd110fdb8961972cd6c12b
Advait <advait.bisram07.ab@gmail.com>
2021-11-15 15:30:36 +0400

    NewVS

--- Hackathon/TravelMauritius/local.js
+++ Hackathon/TravelMauritius/local.js
@@ -1,40 +1,44 @@
 function signup(e) {
   event.preventDefault();
   console.log('working')
-
   var email = document.getElementById('email').value;
   var username = document.getElementById("username").value;
   var pass = document.getElementById("password").value;
-
   var user = {
     email: email,
     username: username,
     password: pass,
   };
-
   var json = JSON.stringify(user);
-  localStorage.setItem(user, json);
+  localStorage.setItem("user", json);
   console.log("user added");
+
+  result.innerHTML = "User Register, Please Log In";
 }
 
 function loginFunc(e) {
   event.preventDefault();
-  var email = document.getElementById("email1").value;
-  var pass = document.getElementById("password1").value;
+  var em = document.getElementById("email1").value;
+  var pa = document.getElementById("password1").value;
   var result = document.getElementById('result');
+  var user = localStorage.getItem(email);
+  var data = JSON.parse(localStorage.getItem('user')) || []
+  console.log(em);
+  console.log(pa);
 
-  var user = localStorage.getItem(username);
-  var data = JSON.parse(user);
 
-  console.log(email);
-  console.log(pass);
-  console.log(data);
+  console.log(data.email);
+  console.log(data.password);
 
-  if (user == null) {
-    result.innerHTML = "Wrong Email, Try Again.";
-  } else if (email == data.email && pass == data.password) {
-    result.innerHTML = "Logged In";
+  if (em == data.email && pa == data.password) {
+
+    console.log("details correct")
+    result.innerHTML = "Logging In";
+    window.location.href = "home.html";
+
   } else {
-    result.innerHTML = "Wrong Password, Try Again";
+    result.innerHTML = "Wrong Email or Password, Try Again."
+
+
   }
 }
---
+++ week 4/Day 1/exercises/exercisesXP.py
@@ -0,0 +1,47 @@
+# Exercise 1
+print ("Hello world!\n" *4)
+
+# Exercise 2
+print ((99**3)*8)
+
+# exercise 3
+print (5 < 3)
+print (3 == 3)
+print ('3' == 3)
+# print ('3' > 3) cant compare str and int
+print ("Hello" == "hello")
+
+# exercise 4
+computer_brand = "Dell"
+print (f"i have a {computer_brand} computer")
+
+# exercise 5
+name = "advait"
+age = "23"
+shoe_size = "44"
+info = (f"Hi, my name is {name}, I'm {age} yrs old and my shoe size is {shoe_size}")
+print (info)
+
+#exercise 6
+a = 5
+b = 6
+if a > b : print("Hello World!")
+else : print("GoodBye World!")
+
+#exercise 7
+# num = int(input("Enter a number:"))
+# if num % num == 0 : print("Even Number!")
+# else : print("Odd Number!")
+
+#exercise 8
+# name = input("Enter a name:")
+#
+# if name == "advait" : print("WOW!, You are unique just like me!")
+# else : print("BOOO!,COMMONER!")
+
+
+#exercise 9
+height = int(input("What's your height (cm)?"))
+if height > 145 : print("You are too tall")
+elif height < 100 : print("You are too short")
+else : print("You may get in")
